21c19773d91bf60b2bfbe77ab03cc1fb5527a9c9
artur_dzidzoiev <artur.dzidzoiev@gmail.com>
2015-03-09 12:43:43 +0200

    sort

--- web/WEB-INF/city.jsp
+++ web/WEB-INF/city.jsp
@@ -1,20 +1,18 @@
 <%@ page language="java" contentType="text/html; charset=utf8"
-         pageEncoding="utf8"%>
-<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+         pageEncoding="utf8" %>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c" %>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
     <meta http-equiv="Content-Type" content="text/html; charset=utf8">
-    <script type="text/javascript" src="${pageContext.request.contextPath}/resources/js/jquery-latest.js"></script>
+    <link rel="stylesheet" type="text/css" href="${pageContext.request.contextPath}/resources/styles/style.css">
+    <script type="text/javascript" src="${pageContext.request.contextPath}/resources/lib/jquery-latest.js"></script>
     <script type="text/javascript" src="${pageContext.request.contextPath}/resources/js/script.js"></script>
-    <script type="text/javascript">
-        $(document).ready(function(){
-            alert('LOL2');
-        });
-    </script>
     <title>Cities</title>
 </head>
 <body>
+
+<div></div>
 
 <form action="${pageContext.request.contextPath}/city/" method="post">
     <table>
@@ -29,21 +27,15 @@
 </form>
 
 <h3>Cities</h3>
-<c:if test="${!empty cityList}">
-    <table class="data" border="0" id="table">
-        <tr>
-            <th>City</th>
-            <th>&nbsp;</th>
-            <th>&nbsp;</th>
-        </tr>
-        <c:forEach items="${cityList}" var="city">
-            <tr>
-                <td>${city.name}</td>
-                <td></td>
-                <td></td>
-            </tr>
-        </c:forEach>
-    </table>
-</c:if>
+<table class="table" border="0" id="table">
+    <tr>
+        <th onclick="sortField('name')">City</th>
+        <th onclick="sortField('population')">Population</th>
+        <th>&nbsp;</th>
+        <th>&nbsp;</th>
+    </tr>
+    <tbody id="table_body">
+    </tbody>
+</table>
 </body>
 </html>
--- web/resources/js/script.js
+++ web/resources/js/script.js
@@ -1,3 +1,85 @@
-$(document).ready(function(){
-	alert('LOL');
+var citiesList;
+var sortParams = {
+    column: null,
+    desc: false,
+    nextOrder: function(column) {
+        console.log(column);
+        console.log(this.column);
+        console.log(this.desc);
+
+        if(column != this.column) {
+            this.column = column;
+            return this.desc = false, this.desc;
+        } else {
+            this.column = column;
+            return this.desc = !this.desc, this.desc;
+        }
+    }
+};
+
+var sortField = function (column) {
+    switch (column) {
+        case 'name' :
+            citiesList.sort(sortBy(column, sortParams.nextOrder(column), function (a) {
+                return a.toUpperCase()
+            }));
+            break;
+        case 'population':
+            citiesList.sort(sortBy(column, !sortParams.nextOrder(column), parseInt));
+            break;
+    }
+
+    clearTable();
+    populateTable(citiesList);
+};
+
+var sortBy = function (field, reverse, primer) {
+
+    var key = primer ?
+        function (x) {
+            return primer(x[field])
+        } :
+        function (x) {
+            return x[field]
+        };
+
+    reverse = !reverse ? 1 : -1;
+
+    return function (a, b) {
+        return a = key(a), b = key(b), reverse * ((a > b) - (b > a));
+    }
+};
+
+var showAll = function (sort) {
+    $.get("/city").done(function (cities) {
+        citiesList = cities;
+        //if(sort) {
+        //    citiesList.sort_by(sort);
+        //}
+        populateTable(citiesList);
+    });
+};
+
+var populateTable = function (cities) {
+    for (var i = 0; i < cities.length; i++) {
+        var city = cities[i];
+        $("#table_body").append("<tr>" +
+        "<td>" + city.name + "</td>" +
+        "<td>" + city.population + "</td>" +
+        "<td>Edit</td>" +
+        "<td>" + "<a href=\"#\" id=\"" + city.id + "\" class=\"del\">Delete</a></td></tr>");
+    }
+};
+
+function clearTable() {
+    $('#table_body').text("");
+}
+
+$(document).ready(function () {
+    showAll();
 });
+
+
+
+
+
