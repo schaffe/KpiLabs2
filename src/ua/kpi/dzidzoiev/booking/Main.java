f12a78e7ef008cb3f615cf6b6aa964dd89a4abad
artur_dzidzoiev <artur.dzidzoiev@gmail.com>
2015-03-12 22:17:32 +0200

    fetching!

--- src/ua/kpi/dzidzoiev/booking/Main.java
+++ src/ua/kpi/dzidzoiev/booking/Main.java
@@ -8,6 +8,7 @@ import ua.kpi.dzidzoiev.booking.controller.db.MySqlConnectionPool;
 import ua.kpi.dzidzoiev.booking.model.City;
 
 import javax.imageio.ImageIO;
+import javax.net.ssl.HttpsURLConnection;
 import javax.net.ssl.SSLContext;
 import javax.net.ssl.SSLSocket;
 import javax.net.ssl.SSLSocketFactory;
@@ -15,46 +16,31 @@ import javax.swing.*;
 import java.awt.*;
 import java.io.*;
 import java.net.InetSocketAddress;
-import java.net.MalformedURLException;
 import java.net.Socket;
 import java.net.URL;
 import java.security.Security;
-import java.util.Arrays;
-import java.util.Collections;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
-        Image image = null;
-       // System.setProperty("jsse.enableSNIExtension", "false");
-// http://stackoverflow.com/questions/7615645/ssl-handshake-alert-unrecognized-name-error-since-upgrade-to-java-1-7-0
-        String urlString = (args.length == 1) ?
-                args[0] : "https://sa-1236541526.ipaas.ipanematech.com/images/logo-1236541526.png";
+        String urlString = "https://sa-1236541526.ipaas.ipanematech.com/images/logo-1236541526.png";
         URL url = new URL(urlString);
-        String _host = url.getHost();
         int _port = 443;
         int _timeout = 300;
 
-        Security.addProvider(
-                new com.sun.net.ssl.internal.ssl.Provider());
-
-        SSLSocketFactory factory =
-                (SSLSocketFactory)SSLSocketFactory.getDefault();
+        Security.addProvider(new com.sun.net.ssl.internal.ssl.Provider());
+        SSLSocketFactory factory = (SSLSocketFactory)SSLSocketFactory.getDefault();
 
         Socket _socket = new Socket();
-        _socket.connect(new InetSocketAddress(_host, _port), _timeout);
+        _socket.connect(new InetSocketAddress(url.getHost(), _port), _timeout);
 
         SSLSocket sslsocket = (SSLSocket) factory.createSocket(_socket, "", _port, true);
         sslsocket.setUseClientMode(true);
         sslsocket.setSoTimeout(_timeout);
         sslsocket.startHandshake();
-
-        InputStream is = sslsocket.getInputStream();
-
-
-        byte[] bytes = getBytes(is);
-        System.out.println(Arrays.toString(bytes));
-        image = ImageIO.read(sslsocket.getInputStream());
+        HttpsURLConnection connection = (HttpsURLConnection) url.openConnection();
+        InputStream is = connection.getInputStream();
+        Image image = ImageIO.read(is);
 
 
         JFrame frame = new JFrame();
@@ -62,20 +48,5 @@ public class Main {
         JLabel label = new JLabel(new ImageIcon(image));
         frame.add(label);
         frame.setVisible(true);
-    }
-
-    static byte[] getBytes(InputStream is) throws IOException {
-        ByteArrayOutputStream buffer = new ByteArrayOutputStream();
-
-        int nRead;
-        byte[] data = new byte[16384];
-
-        while ((nRead = is.read(data, 0, data.length)) != -1) {
-            buffer.write(data, 0, nRead);
-        }
-
-        buffer.flush();
-
-        return buffer.toByteArray();
     }
 }
